69fc8d1acd2b9d8c3626683d6dcc313159ec061b
Keith Ratner <keith.ratner@gmail.com>
2023-07-12 21:31:02 -0700

    New item

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,6 +6,7 @@ const Main = () => {
     <main className="main">
         <h2>The Latest</h2>
         <ACastAudio audioSource="https://embed.acast.com/$/62a222737c02140013aa4c03/episode-1449-the-united-states-of-conspiracy-w-robert-guffey" />
+        <YouTubeVideo videoId="imAGg5Lj8lc" />
         <YouTubeVideo videoId="XoVxec0pvz4" />
         <YouTubeVideo videoId="cG8h_FpT5FM" />
     </main>
