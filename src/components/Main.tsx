0fa787103a0da2ff0d1c18cc92b3cd05fd6e6816
Keith Ratner <keith.ratner@gmail.com>
2023-07-16 20:40:51 -0700

    Video

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,11 +3,11 @@ import YouTubeVideo from "./YouTubeVideo";
 
 const Main = () => {
 
-
   return (
     <main className="main">
         <h2>The Latest</h2>
         <ACastAudio audioSource="https://embed.acast.com/$/62a222737c02140013aa4c03/episode-1449-the-united-states-of-conspiracy-w-robert-guffey" />
+        <YouTubeVideo videoId="RXY268ocwp0" />
         <YouTubeVideo videoId="g2C3mt36U1s" />
         <YouTubeVideo videoId="wktuUgo_zhE" />
         <YouTubeVideo videoId="imAGg5Lj8lc" />
