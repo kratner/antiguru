05cf80dfdcc90e132ac8ee6314bf6eca444fc18e
Keith Ratner <keith.ratner@gmail.com>
2023-08-14 16:05:12 -0700

    GoogleAnalytics.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./styles/main.scss";
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import GoogleTag from './components/GoogleTag';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import GoogleTag from "./components/GoogleTag";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 function App() {
   return (
     <div className="App">
-      <GoogleTag trackingId='G-502EZJ5VN6' />
+      <GoogleTag trackingId="G-502EZJ5VN6" />
       <Header />
       <Main />
       <Footer />
+      <GoogleAnalytics measurementId="G-502EZJ5VN6" />
     </div>
   );
 }
